5b65f5b88c65b71ba2558f9a7c43a6378d636046
WeversonL <weversonlemos10@protonmail.com>
2023-07-05 18:15:19 -0300

    fix: Adjust cors endpoint

--- src/main/java/com/cepsearch/config/WebConfig.java
+++ src/main/java/com/cepsearch/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/cep/**")
+        registry.addMapping("/cep/**")
                 .allowedMethods("GET");
     }
 
